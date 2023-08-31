787b2932572ae80efbc1a16f8c9ad79363ccebeb
Luiz carlos <luizandreoletti@gmail.com>
2023-08-31 16:26:27 -0300

    primeiro formulário

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,21 @@
-document.getElementById('chamadoForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    // Obter os valores dos campos
-    var nome = document.getElementById('nome').value;
-    var email = document.getElementById('email').value;
-    var mensagem = document.getElementById('mensagem').value;
-    
-    // Enviar os dados para o servidor (simulação)
-    // Aqui você pode adicionar a lógica para enviar os dados para o backend
-    console.log('Nome: ' + nome);
-    console.log('Email: ' + email);
-    console.log('Mensagem: ' + mensagem);
-    
-    // Limpar os campos do formulário
-    document.getElementById('nome').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('mensagem').value = '';
-  });
+const menuToggle = document.getElementById('menu-toggle');
+    const nav = document.querySelector('nav');
+
+    menuToggle.addEventListener('change', () => {
+      nav.classList.toggle('active', menuToggle.checked);
+    });
+
+    const chamadoForm = document.getElementById('chamado-form');
+
+    chamadoForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      // Aqui você pode adicionar a lógica para processar o formulário
+      // Por exemplo, enviar os dados para um servidor ou realizar outras ações.
+    });
+
+    const menuIcon = document.getElementById('menuIcon');
+const sidebar = document.getElementById('sidebar');
+
+menuIcon.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
