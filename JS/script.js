6f9762ad0d61525ec917f6ef1cf4149aace4ebe0
Luiz carlos <luizandreoletti@gmail.com>
2023-09-30 22:14:14 -0300

    ajustes

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,30 @@ const sidebar = document.getElementById('sidebar');
 menuIcon.addEventListener('click', () => {
   sidebar.classList.toggle('active');
 });
+
+/*-------chamado-----*/
+const saveBtn = document.getElementById('registrar');
+const cancelBtn = document.getElementById('cancel-btn');
+const firstNameInput = document.getElementById('nome');
+const lastNameInput = document.getElementById('sobrenome');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('senha');
+const errorMsg = document.getElementById('error-msg');
+
+saveBtn.addEventListener('click', () => {
+  editBtn.classList.remove('hide');
+  saveBtn.classList.add('hide');
+  cancelBtn.classList.add('hide');
+  firstNameInput.disabled = true;
+  lastNameInput.disabled = true;
+  emailInput.disabled = true;
+  passwordInput.disabled = true;
+  errorMsg.classList.add('hide');
+});
+
+$("#registrar").click(function(){
+  $("p").hide(0);
+  $("#editForm").show(0);
+  $("form input").prop('disabled', true);
+  $(this).hide(0);
+});
